be087d9bc319a4329781637d7911922c103e7207
AtwoodDeng <dengjdtsinghua@gmail.com>
2015-11-02 14:37:44 -0800

    for save

---
+++ Assets/MAsset/MScript/Main.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Main : MonoBehaviour {
+
+	public Main() { _Instance = this;}
+	public static Main Instance { get{ return _Instance; } }
+	private static Main _Instance;
+
+
+	[SerializeField] List<GameObject> Images;
+	[SerializeField] float SwitchTime = 1f;
+	[SerializeField] GameObject TempRoot;
+	// Use this for initialization
+	void Awake () 
+	{
+		StartCoroutine(SwitchImageCall());
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+
+	IEnumerator SwitchImageCall()
+	{
+		while(true)
+		{
+			SwitchImage();
+			yield return new WaitForSeconds(SwitchTime);
+		}
+	}
+
+	void SwitchImage()
+	{
+		for ( int i = 0 ; i < Images.Count -1 ; ++i )
+		{
+			if ( Images[i].activeSelf )
+			{
+				Images[i].SetActive(false);
+				Images[i+1].SetActive(true);
+				Debug.Log("set!!!");
+				return ;
+			}
+		}
+		if ( Images[Images.Count-1].activeSelf )
+		{
+			Images[Images.Count-1].SetActive(false);
+			Images[0].SetActive(true);
+			Debug.Log("set last!!!");
+		}
+		Debug.Log("Switch");
+	}
+
+	public GameObject GetTemperImage()
+	{
+		for ( int i = 0 ; i < Images.Count ; ++i )
+		{
+			if ( Images[i].activeSelf )
+			{
+				return Images[i];
+			}
+		}
+		return null;
+	}
+
+	public GameObject GetTempRoot()
+	{
+		return TempRoot;
+	}
+}
---
+++ Assets/MAsset/MScript/SDrableItem.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public class SDrableItem : UIDragDropItem {
+
+	Vector3 oriPos;
+
+	void Awake()
+	{
+		base.Awake();
+		oriPos = transform.position;
+	}
+
+	protected override void OnDragDropStart ()
+	{
+		base.OnDragDropStart ();
+		this.transform.parent = Main.Instance.GetTempRoot().transform;
+		GetComponent<Rigidbody>().useGravity = false;
+		GetComponent<Collider>().isTrigger = true;
+	}
+
+	protected override void OnDragDropMove (Vector2 delta)
+	{
+		base.OnDragDropMove (delta);
+	}
+
+	protected override void OnDragDropRelease (GameObject surface)
+	{
+		base.OnDragDropRelease (surface);
+		this.transform.parent = Main.Instance.GetTemperImage().transform;
+		GetComponent<Rigidbody>().useGravity = true;
+		GetComponent<Collider>().isTrigger = false;
+	}
+}
